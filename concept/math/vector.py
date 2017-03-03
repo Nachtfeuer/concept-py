e20a7a9711938c6b240377c7ea308138d4938ffd
Thomas Lehmann <thomas.lehmann.private@gmail.com>
2017-03-03 05:42:27 +0100

    Math 2d vector.

---
+++ concept/math/vector.py
@@ -0,0 +1,176 @@
+"""
+   Math vector 2d and 3d.
+
+.. module:: vector
+    :platform: Unix, Windows
+    :synopis: math vector 2d and 3d.
+
+.. moduleauthor:: Thomas Lehmann <thomas.lehmann.private@googlemail.com>
+
+   =======
+   License
+   =======
+   Copyright (c) 2017 Thomas Lehmann
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this
+   software and associated documentation files (the "Software"), to deal in the Software
+   without restriction, including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+   to whom the Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all copies
+   or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+   INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+   DAMAGES OR OTHER LIABILITY,
+   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+import math
+
+
+class Vector2d(object):
+    """
+    Math vector 2d implementation.
+
+    >>> a = Vector2d(3, 4)
+    >>> b = Vector2d(1, 3)
+    >>> a + b
+    Vector2d(x=4, y=7)
+    >>> a - b
+    Vector2d(x=2, y=1)
+    >>> a.scalar_product(b)
+    15.0
+    >>> a.length()
+    5.0
+    >>> a.scaled(2)
+    Vector2d(x=6, y=8)
+    >>> a.normalized().length()
+    1.0
+    >>> 180.0 / math.pi * Vector2d(0.0, 1.0).angle(Vector2d(1.0, 0.0))
+    90.0
+    >>> v = Vector2d(0.0, 1.0).rotated(-math.pi/180.0 * 90.0)
+    >>> abs(v.x - 1.0) < 1e-10 and abs(v.y) < 1e-10
+    True
+    """
+
+    def __init__(self, x=0.0, y=0.0):
+        """Initialize vector."""
+        self.x = float(x)
+        self.y = float(y)
+
+    def __repr__(self):
+        """:returns: String representation of the given vector 2d instance."""
+        return "Vector2d(x=%(x)g, y=%(y)g)" % self.__dict__
+
+    def __add__(self, other):
+        """
+        Sum of two vectors.
+
+        $\vec{a} + \vec{b} = \begin{pmatrix}a_x + b_x\\a_y + b_y\end{pmatrix}$
+        """
+        return Vector2d(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        """
+        Subtraction of two 2d vectors.
+
+        $\vec{a} - \vec{b} = \begin{pmatrix}a_x - b_x\\a_y - b_y\end{pmatrix}$
+        :returns: new 2d vector instance.
+        """
+        return Vector2d(self.x - other.x, self.y - other.y)
+
+    def scalar_product(self, other):
+        """
+        Scalar (dot) product of two 2d vectors.
+
+        :param other: another 2d vector instance
+        :returns: scalar product of two 2d vectors.
+        """
+        assert isinstance(other, Vector2d)
+        return self.x * other.x + self.y * other.y
+
+    def cross_product(self, other):
+        """
+        Cross product of two vectors.
+
+        :param other: another 2d vector instance
+        :returns: float value representing cross product of two vectors.
+        """
+        assert isinstance(other, Vector2d)
+        return self.x * other.y - self.y * other.x
+
+    def length(self):
+        """:returns: length of the vector."""
+        return math.sqrt(self.scalar_product(self))
+
+    def scale(self, factor):
+        """
+        Scale current vector by given factor.
+
+        :param factor: integer or float value expected
+        """
+        assert isinstance(factor, int) or isinstance(factor, float)
+        self.x *= factor
+        self.y *= factor
+
+    def scaled(self, factor):
+        """
+        Provide scaled vector by given factor; current instance will not be changed.
+
+        :param factor: integer or float value expected
+        :return: new scaled 2d vector instance
+        """
+        assert isinstance(factor, int) or isinstance(factor, float)
+        return Vector2d(self.x * factor, self.y * factor)
+
+    def normalized(self):
+        """
+        Provide a normalized vector (vector length is 1).
+        """
+        return self.scaled(1.0 / self.length())
+
+    def angle(self, other):
+        """
+        Calculate angle between two vectors.
+
+        :param other: another 2d vector
+        :returns: angle between two vectors (unit: rad).
+        """
+        assert isinstance(other, Vector2d)
+        return math.atan2(self.y, self.x) - math.atan2(other.y, other.x)
+
+    def turned_left(self):
+        """
+        Provide vector rotated 90 degrees left.
+
+        :returns: new 2d vector instance.
+        """
+        return Vector2d(-self.y, self.x)
+
+    def turned_right(self):
+        """
+        Provide vector rotated 90 degrees right.
+
+        :returns: new 2d vector instance.
+        """
+        return Vector2d(self.y, -self.x)
+
+    def rotated(self, rotation_angle):
+        """
+        Provide angle rotated by given angle.
+
+        :param rotation_angle: angle to use for rotation (unit: rad)
+        :returns: new 2d vector instance.
+        """
+        return Vector2d(self.x * math.cos(rotation_angle) - self.y * math.sin(rotation_angle),
+                        self.x * math.sin(rotation_angle) + self.y * math.cos(rotation_angle))
+
+    def __eq__(self, other):
+        """Comparing two vectors to be equal."""
+        if isinstance(other, Vector2d):
+            return abs(self.x - other.x) < 1e-10 and abs(self.y - other.y) < 1e-10
+        return False
---
+++ tests/test_vector_2d.py
@@ -0,0 +1,110 @@
+"""
+   =======
+   License
+   =======
+   Copyright (c) 2017 Thomas Lehmann
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this
+   software and associated documentation files (the "Software"), to deal in the Software
+   without restriction, including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+   to whom the Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all copies
+   or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+   INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+   DAMAGES OR OTHER LIABILITY,
+   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+# pylint: disable=R0201
+import math
+import unittest
+from hamcrest import assert_that, equal_to
+from concept.math.vector import Vector2d
+
+
+class TestVector2d(unittest.TestCase):
+    """ Testing math 2d vector. """
+
+    def test_init(self):
+        """Testing of method Vector2d.__init__."""
+        assert_that(Vector2d(1.0, 2.0).x, equal_to(1.0))
+        assert_that(Vector2d(1.0, 2.0).y, equal_to(2.0))
+        assert_that(Vector2d(), equal_to(Vector2d(0.0, 0.0)))
+
+    def test_repr(self):
+        """Testing of method Vector2d.__repr__."""
+        assert_that(str(Vector2d(1.2, 3.4)), equal_to("Vector2d(x=1.2, y=3.4)"))
+
+    def test_add(self):
+        """Testing of method Vector2d.__add__."""
+        assert_that(Vector2d(1.0, 2.0) + Vector2d(3.0, 4.0), equal_to(Vector2d(4.0, 6.0)))
+
+    def test_sub(self):
+        """Testing of method Vector2d.__sub__."""
+        assert_that(Vector2d(1.0, 5.0) - Vector2d(3.0, 4.0), equal_to(Vector2d(-2.0, 1.0)))
+
+    def test_scalar_product(self):
+        """Testing of method Vector2d.scalar_product."""
+        assert_that(Vector2d(2.0, 5.0).scalar_product(Vector2d(3.0, 4.0)), equal_to(26))
+
+    def test_length(self):
+        """Testing of method Vector2d.length."""
+        assert_that(Vector2d(3.0, 4.0).length(), equal_to(5.0))
+
+    def test_scaled(self):
+        """Testing of method Vector2d.scaled."""
+        vec = Vector2d(3.0, 4.0)
+        assert_that(vec.scaled(2), equal_to(Vector2d(6.0, 8.0)))
+        assert_that(vec, equal_to(Vector2d(3.0, 4.0)))
+
+    def test_scale(self):
+        """Testing of method Vector2d.scale."""
+        vec = Vector2d(3.0, 4.0)
+        vec.scale(2.0)
+        assert_that(vec, equal_to(Vector2d(6.0, 8.0)))
+
+    def test_rotated(self):
+        """Testing of method Vector2d.rotated."""
+        vec_a = Vector2d(1.0, 0.0)
+        vec_b = vec_a.rotated(math.pi / 180.0 * 90)
+
+        assert_that(abs(vec_b.x) < 1e-10, equal_to(True))
+        assert_that(abs(vec_b.y - 1.0) < 1e-10, equal_to(True))
+
+    def test_turned_left(self):
+        """Testing of method Vector2d.turned_left."""
+        assert_that(Vector2d(1.0, 0.0).turned_left(), equal_to(Vector2d(0.0, 1.0)))
+        assert_that(Vector2d(0.0, 1.0).turned_left(), equal_to(Vector2d(-1.0, 0.0)))
+        assert_that(Vector2d(-1.0, 0.0).turned_left(), equal_to(Vector2d(0.0, -1.0)))
+        assert_that(Vector2d(0.0, -1.0).turned_left(), equal_to(Vector2d(1.0, 0.0)))
+
+    def test_turned_right(self):
+        """Testing of method Vector2d.turned_right."""
+        assert_that(Vector2d(1.0, 0.0).turned_right(), equal_to(Vector2d(0.0, -1.0)))
+        assert_that(Vector2d(0.0, -1.0).turned_right(), equal_to(Vector2d(-1.0, 0.0)))
+        assert_that(Vector2d(-1.0, 0.0).turned_right(), equal_to(Vector2d(0.0, 1.0)))
+        assert_that(Vector2d(0.0, 1.0).turned_right(), equal_to(Vector2d(1.0, 0.0)))
+
+    def test_angle(self):
+        """Testing of method Vector2d.angle."""
+        angle_a = Vector2d(0.0, 1.0).angle(Vector2d(1.0, 0.0)) * 180.0 / math.pi
+        angle_b = Vector2d(1.0, 0.0).angle(Vector2d(0.0, 1.0)) * 180.0 / math.pi
+        assert_that(abs(angle_a - 90.0) <= 1e-10, equal_to(True))
+        assert_that(abs(angle_b + 90.0) <= 1e-10, equal_to(True))
+
+    def test_normalized(self):
+        """Testing of method Vector2d.normalized."""
+        normalized_vec_a = Vector2d(10.0, 0).normalized()
+        normalized_vec_b = Vector2d(0.0, 10.0).normalized()
+        assert_that(normalized_vec_a, equal_to(Vector2d(1.0, 0.0)))
+        assert_that(normalized_vec_b, equal_to(Vector2d(0.0, 1.0)))
+
+    def test_cross_product(self):
+        """Testing of method Vector2d.cross_product."""
+        assert_that(Vector2d(2.0, 5.0).cross_product(Vector2d(3.0, 4.0)), equal_to(-7.0))
